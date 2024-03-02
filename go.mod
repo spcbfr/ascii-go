9a85b000b6be7f6a984cc601b0e10876925d4673
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2024-03-02 21:14:26 +0100

    Initial Implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module octopi
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"os"
+)
+
+func main() {
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+	image, err := os.Open("./yusuf.png")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer image.Close()
+	pixels, err := getPixels(image)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ascii := getAscii(pixels)
+	f, err := os.Create("./ascii-result")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	_, err = f.WriteString(ascii)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+
+}
+
+func getAsciiChar(brightness int) string {
+	const codes = "Ñ@#W$9876543210?!abc;:+=-,._          "
+	charCode := int(mapRange(float64(brightness), 0, 255, float64(len(codes)), 0))
+	return string(codes[charCode])
+
+}
+
+func getAscii(data [][]Pixel) string {
+	var result string
+	for y := 0; y < len(data); y++ {
+		var line string
+		for x := 0; x < len(data[y]); x++ {
+
+			line = line + getAsciiChar(brightness(data[y][x]))
+		}
+		result = result + line + "\n"
+	}
+	return result
+}
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	bounds := img.Bounds()
+	var pixels [][]Pixel
+	width, height := bounds.Max.X, bounds.Max.Y
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+	return pixels, nil
+}
+
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257)}
+}
+func brightness(pixel Pixel) int {
+	// placeholder calculation method
+	return (pixel.R + pixel.G + pixel.B) / 3
+}
+
+type Pixel struct {
+	R int
+	G int
+	B int
+}
+
+func mapRange(value, minInput, maxInput, minOutput, maxOutput float64) float64 {
+	// Clamping the value to the input range
+	if value < minInput {
+		value = minInput
+	} else if value > maxInput {
+		value = maxInput
+	}
+
+	// Calculate the proportion of the value in the input range
+	proportion := (value - minInput) / (maxInput - minInput)
+
+	// Map the proportion to the output range
+	newValue := proportion*(maxOutput-minOutput) + minOutput
+
+	return newValue
+}
